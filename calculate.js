fef6a4200e0db799eeee92a93ef4ff8e2090c180
Pedrisimo <artyom.kulyagin@gmail.com>
2016-05-02 17:32:36 +0300

    Task implementation
    All the subtasks were implemented

---
+++ calculate.js
@@ -0,0 +1,142 @@
+document.getElementById('calculate_button').addEventListener('click', calculateExp);
+
+function calculateExp()
+{
+	if(ValidateAll() == true)
+	{
+		var Data = document.getElementById('expression_input').value;
+		var expr_array = expToArray(Data);
+		var formatted_array = formatExpr(expr_array);
+		var exp_result = executeExpr(formatted_array);
+		console.log(exp_result);
+		setValidateMessage(exp_result);
+	}
+}
+function expToArray(expr)
+{
+	var exp_pattern = new RegExp(/[\d+\+\-\:\x\(\)]/g);
+	var exp_mas = expr.match(exp_pattern);
+	var mas = [];
+	var digit_flag;
+	for(var i=0; i < exp_mas.length; i++)
+	{
+		if(exp_mas[i].match(/\d/) != null)
+		{
+			if(digit_flag == true)
+			{
+				mas[mas.length-1] = mas[mas.length-1].concat(exp_mas[i]);
+			}
+			else
+			{
+				mas.push(exp_mas[i]);
+			}
+			digit_flag = true;
+		}
+		else
+		{
+			mas.push(exp_mas[i]);
+			digit_flag = false;
+		}
+	}
+	exp_mas = mas;
+	return exp_mas;
+}
+
+function formatExpr(expr_array)
+{
+	var output_array=[];
+	var stack =[];
+	for(var i=0; i < expr_array.length; i++)
+	{
+		if(expr_array[i].match(/\d/) != null)
+		{
+			output_array.push(expr_array[i]);
+			non_digit_flag = false;
+		}
+		else
+		{
+			if(expr_array[i].match(/[\:\x\-\+\(]/) != null)
+			{
+				if(stack.length > 0)
+				{
+					if((stack[stack.length-1].match(/[\:\x]/) != null && expr_array[i].match(/[\:\x]/) != null) || (stack[stack.length-1].match(/[\-\+]/) != null && expr_array[i].match(/[\-\+]/) != null) || (stack[stack.length-1].match(/[\:\x]/) != null && expr_array[i].match(/[\-\+]/) != null))
+					{
+						output_array.push(stack[stack.length-1]);
+						stack.pop();
+						stack.push(expr_array[i]);
+					}
+					else
+					{
+						stack.push(expr_array[i]);
+					}
+				}
+				else
+				{
+					stack.push(expr_array[i]);
+					
+				}
+			}
+			else
+			{
+				for(var s = 0; s <= stack.length; s++)
+				{
+					if(stack[stack.length-1].match(/\(/) != null)
+					{
+						stack.pop();
+						break;
+					}
+					else
+					{
+						output_array.push(stack[stack.length-1]);
+						stack.pop();
+					}
+				}
+
+			}
+		}
+	}
+	while(stack.length > 0)
+	{
+		output_array.push(stack[stack.length-1]);
+		stack.pop();
+	}
+	return output_array;
+}
+
+function executeExpr(formatted_array)
+{
+	var resultString;
+	var result_array =[];
+	for(var i = 0; i < formatted_array.length; i++)
+	{
+		if(formatted_array[i].match(/\d+/) != null)
+		{
+			result_array.push(parseInt(formatted_array[i]));
+		}
+		else
+		{
+			if(formatted_array[i].match(/\-/) != null)
+			{
+				result_array[result_array.length-2] = result_array[result_array.length-2]-result_array[result_array.length-1];
+				result_array.pop();
+			}
+			else if(formatted_array[i].match(/\+/) != null)
+			{
+				result_array[result_array.length-2] = result_array[result_array.length-2]+result_array[result_array.length-1];
+				result_array.pop();
+			}
+			else if(formatted_array[i].match(/\x/) != null)
+			{
+				result_array[result_array.length-2] = result_array[result_array.length-2]*result_array[result_array.length-1];
+				result_array.pop();
+			}
+			else if(formatted_array[i].match(/\:/) != null)
+			{
+				result_array[result_array.length-2] = result_array[result_array.length-2]/result_array[result_array.length-1];
+				result_array.pop();
+			}
+
+		}
+	}
+	return result_array[result_array.length-1];
+}
---
+++ validate.js
@@ -0,0 +1,159 @@
+document.getElementById('validate_buttton').addEventListener('click', ValidateAll);
+
+function ValidateAll()
+{
+	var Data = document.getElementById('expression_input').value;
+	if(Data == "")
+	{
+		setValidateMessage("validateError");
+	}
+	else
+	{
+//		Data = formatString(Data); //unused
+		if(validateBrackets(Data) != true || validatePattern(Data) != true || validatePatternEnd(Data) != true || validateMinCon(Data) != true || validateEmptyBr(Data) != true)
+		{
+			setValidateMessage("validateError");
+			return false;
+		}
+		else
+		{
+			setValidateMessage("validateSuccess");
+			return true;	
+		}
+	}
+}
+
+function formatString(Data)
+{
+
+	var tempData = Data.replace(":", "/");
+	tempData = tempData.replace("x", "*");
+	tempData = tempData.replace(/\s/g, "");
+	document.getElementById('expression_input').value = tempData;
+	return tempData;
+}
+
+function validateBrackets(Data)
+{
+
+	var leftBracketNum = (Data.match(/\(/g));
+	var rigthBracketNum = (Data.match(/\)/g));
+	if(leftBracketNum != null && rigthBracketNum != null)
+	{
+		if(leftBracketNum.length != rigthBracketNum.length)
+		{
+			return false;
+		}
+		else
+		{
+			return true;
+		}
+	}
+	else if(leftBracketNum == null && rigthBracketNum != null)
+	{
+		return false;
+	}
+	else if(rigthBracketNum == null && leftBracketNum != null)
+	{
+		return false;
+	}
+	else
+	{
+		return true;
+	}
+}
+function validatePattern(Data)
+{
+	
+	console.log("String before validatePattern: "+Data);
+	var validPattern = new RegExp(/[0-9\+\-\:\x\(\)]/ig);
+	var tempData = Data.replace(validPattern, "");
+	console.log("String before validatePattern: "+ tempData);
+	if(tempData.length > 0)
+	{
+		return false;
+	}
+	else
+	{
+		return true;
+	}
+}
+function validatePatternEnd(Data)
+{
+	console.log("String before validatePatternEnd: "+ Data);
+	var validPattern = new RegExp(/([\+\-\x\:]{2,})/g);
+	var validPatternEnd = new RegExp(/^[\+\-\x\:]|[\+\-\x\:]$/g);
+	var tempData = Data;
+	tempData = Data.match(validPattern);
+	console.log("String after validatePatternEnd: "+ tempData);
+	if(tempData == null)
+	{
+		tempData = Data.match(validPatternEnd);
+		if(tempData == null)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	else
+	{
+		return false;
+	}
+}
+
+function validateEmptyBr(Data)
+{
+	var EmptyPattern = new RegExp(/\(\)/g);
+	var tempData = Data;
+	tempData = Data.match(EmptyPattern);
+	if(tempData != null)
+	{
+		return false;
+	}
+	else
+	{
+		return true;
+	}
+}
+
+function validateMinCon(Data)
+{
+	var MinPattern = new RegExp(/(\d+(\+|\-|\x|\:)\d+)|((\+|\-|\x|\:)\d+)|(\d+(\+|\-|\x|\:))/g);
+	var tempData = Data;
+	tempData = Data.match(MinPattern);
+	if(tempData != null)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+
+function setValidateMessage(messageType)
+{
+
+	if(messageType == "validateSuccess")
+	{
+		document.getElementById('validateMessage').style.width="200px";
+		document.getElementById('validateMessage').style.background = "green";
+		document.getElementById('validateMessage').innerHTML='<font color = "white">Expression is valid!</fonr>';
+	}
+	else if(messageType == "validateError")
+	{
+		document.getElementById('validateMessage').style.width="200px";
+		document.getElementById('validateMessage').style.background = "red";
+		document.getElementById('validateMessage').innerHTML='<font color="yellow">Expression is not valid!</font>';
+	}
+	else
+	{
+		document.getElementById('validateMessage').style.width="200px";
+		document.getElementById('validateMessage').style.background = "blue";
+		document.getElementById('validateMessage').innerHTML='<font color="white">'+ messageType+ '</font>';
+	}
+}
