1f380e41e0420edf4f1acd259887a770dd4fca93
syshchyk <crushcat@gmail.com>
2016-05-02 23:19:08 +0300

    task complete

--- calc.js
+++ calc.js
@@ -10,28 +10,65 @@ Array.prototype.clean = function() {
     }
     return this;
 }
-function calculate(){
-	
-	var str = document.getElementById('expr').value;
-	postfix = infixToPostfix(str);
-	result = solvePostfix(postfix);
+var errorMessage;
+var postfix;
 
-	document.getElementById('val').innerHTML = postfix;
-	document.getElementById('res').innerHTML = result;
+document.getElementById('validate').addEventListener('click', validate);
+document.getElementById('calculate').addEventListener('click', calculate);
+var rd = document.getElementById('msg');
+
+function validate(){
+    var str = document.getElementById('expr').value;
+    var errLvl = 0;
+    if(str.length>0){
+        errorMessage = "";
+        postfix = "";        
+        if(infixToPostfix(str)<1){
+            setValOk("expression is valid");
+        }
+        else {
+            setValErr(errorMessage);
+            errLvl=1;        
+        }
+    }
+    else {
+        setValErr('expression is empty'); 
+        errLvl=1;    
+    }
+    return errLvl
+
+}
+function calculate(){
+	if(validate()===0){
+        setCalcRes(solvePostfix(postfix));
+    }
+}
+
+function setCalcRes (str) {
+     rd.style.width='200px';
+     rd.style.background='navy';
+     rd.style.color = 'white';
+     rd.innerHTML = str;
+}
+
+function setValOk (str) {
+     rd.style.width='200px';
+     rd.style.background='green';
+     rd.style.color = 'white';
+     rd.innerHTML = str;  
+}
+
+function setValErr (str) {
+     rd.style.width='200px';
+     rd.style.background='red';
+     rd.style.color = 'yellow';
+     rd.innerHTML = str; 
 }
 
 function infixToPostfix (infix) {
-	//TODO: add () validation
-	//TODO: add other(?) validation
-    var errMsgB = "";
-    var errMsgE = "";
-    var okMsgB = "";
-    var okMsgE = "";
-    var resMsgB = "";
-    var resMsgE = "";
-    var VerrMsg = "";
     var outputQueue = "";
     var operatorStack = [];
+    var errLevel = 0;
     var operators = {
         
         "x": {
@@ -43,7 +80,7 @@ function infixToPostfix (infix) {
         ":": {
             precedence: 3
         },
-                "+": {
+        "+": {
             precedence: 2
         },
         "-": {
@@ -51,37 +88,59 @@ function infixToPostfix (infix) {
         }
     }
     infix = infix.replace(/\s+/g, "");
+    if (infix.replace(/([0-9\+\-\x\х\\:\(\)])/ig, '')!==''){
+        errorMessage += " unexpected chars;"; 
+        errLevel++;
+    }
     infix = infix.split(/([\+\-\x\х\\:\(\)])/).clean();
     for(var i = 0; i < infix.length; i++) {
         var token = infix[i];
         if(token.isNumeric()) {
             outputQueue += token + " ";
         } else if("xх:+-".indexOf(token) !== -1) {
-            var op1 = token;
-            var op2 = operatorStack[operatorStack.length - 1];
-            while("xх:+-".indexOf(op2) !== -1 && ((operators[op1].precedence <= operators[op2].precedence))) {
-                outputQueue += operatorStack.pop() + " ";
-                op2 = operatorStack[operatorStack.length - 1];
+            if("xх:+-".indexOf(infix[i-1]) !== -1){
+                errorMessage += " two opeators successively;"; 
+                errLevel++;
             }
-            operatorStack.push(op1);
+            else if (i===0||i===infix.length-1) {
+                errorMessage += " leading or ending operator;"; 
+                errLevel++;   
+            }
+            else{
+                var op1 = token;
+                var op2 = operatorStack[operatorStack.length - 1];
+                while("xх:+-".indexOf(op2) !== -1 && ((operators[op1].precedence <= operators[op2].precedence))) {
+                    outputQueue += operatorStack.pop() + " ";
+                    op2 = operatorStack[operatorStack.length - 1];
+                }
+                operatorStack.push(op1);
+            }
         } else if(token === "(") {
             operatorStack.push(token);
         } else if(token === ")") {
-            while(operatorStack[operatorStack.length - 1] !== "(") {
+            while(operatorStack[operatorStack.length - 1] !== "("&&operatorStack.length>0) {
                 outputQueue += operatorStack.pop() + " ";
             }
+            if (operatorStack[operatorStack.length - 1] === "("){
             operatorStack.pop();
+            }
+            else {
+                errorMessage += " missing opening bracket;"; 
+                errLevel++;
+            }
         }
     }
     while(operatorStack.length > 0) {
         if (operatorStack[operatorStack.length - 1] === "("){
-        
+            errorMessage += " missing closing bracket;";
+            errLevel++;
+
         }
         outputQueue += operatorStack.pop() + " ";
     }
-    return outputQueue;
+    postfix = outputQueue;
+    return errLevel;
 }
-
 
 function solvePostfix (postfix) {
         var resultStack = [];
@@ -113,5 +172,3 @@ function solvePostfix (postfix) {
             return resultStack.pop();
         }
 }
-
-
